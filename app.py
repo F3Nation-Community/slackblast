442f31ba162948f6dacee5fb4911160e5582cc60
Trent Jones <JonesTD@legal.regn.net>
2020-12-16 17:05:23 -0500

    flask app for prod

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 # Import the async app instead of the regular one
+from slack_bolt.adapter.flask import SlackRequestHandler
+from flask import Flask, request
 from slack_bolt.async_app import AsyncApp
 import logging
 from decouple import config
-
 
 logging.basicConfig(level=logging.DEBUG)
 
 
 app = AsyncApp(
     token=config('SLACK_BOT_TOKEN'),
-    signing_secret=config('SLACK_SIGNING_SECRET')
+    signing_secret=config('SLACK_SIGNING_SECRET'),
+    logger=logging.Logger
 )
 
 
@@ -30,5 +32,20 @@ async def command(ack, body, respond):
     await ack()
     await respond(f"Hello <@{body['user_id']}>!")
 
-if __name__ == "__main__":
-    app.start(config('PORT'))
+# Initialize the Flask app
+
+flask_app = Flask(__name__)
+handler = SlackRequestHandler(app)
+
+# Register routes to Flask app
+
+
+@flask_app.route("/")
+def hellow_world():
+    return "ok"
+
+
+@flask_app.route("/slack/events", methods=["POST"])
+def slack_events():
+    # handler runs App's dispatch method
+    return handler.handle(request)
