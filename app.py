50157ae66b1a6e2279309c7e157413ba01817c9b
Trent Jones <trenton.jones@gmail.com>
2020-12-16 21:09:52 -0500

    modal

--- app.py
+++ app.py
@@ -27,10 +27,165 @@ def event_test(body, say, logger):
 
 
 @slack_app.command("/backblast")
-def command(ack, body, respond, logger):
+def command(ack, body, respond, client, logger):
     ack()
-    logger.info(body)
-    respond(f"Hello <@{body['user_id']}>!")
+    res = client.views_open(
+        trigger_id=body["trigger_id"],
+        view={
+            "type": "modal",
+            "callback_id": "backblast-id",
+            "title": {
+                "type": "plain_text",
+                "text": "Create a Backblast"
+            },
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit"
+            },
+            "blocks": [
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "title",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Snarky Title?"
+                        }
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Title"
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Choose an AO",
+                            "emoji": True
+                        },
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": True
+                                },
+                                "value": "value-0"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": True
+                                },
+                                "value": "value-1"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "*this is plain_text text*",
+                                    "emoji": True
+                                },
+                                "value": "value-2"
+                            }
+                        ],
+                        "action_id": "static_select-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Workout",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "datepicker",
+                        "initial_date": "1990-04-28",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a date",
+                            "emoji": True
+                        },
+                        "action_id": "datepicker-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Workout Date",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "*The Q*"
+                    },
+                    "accessory": {
+                        "type": "users_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select a PAX",
+                            "emoji": True
+                        },
+                        "action_id": "users_select-action"
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "multi_users_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select users",
+                            "emoji": True
+                        },
+                        "action_id": "multi_users_select-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "The Pax",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "multiline": True,
+                        "action_id": "plain_text_input-action"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "The Details",
+                        "emoji": True
+                    }
+                }
+            ]
+        },
+    )
+    logger.info(res)
+
+
+@slack_app.view("backblast-id")
+def view_submission(ack, body, logger, client):
+    ack()
+    logger.info(body["view"]["state"]["values"])
+    user = body["user"]["id"]
+    msg = ""
+    try:
+        # Save to DB
+        msg = f"Your submission of blah was successful"
+    except Exception as e:
+        # Handle error
+        msg = "There was an error with your submission"
+    finally:
+        # Message the user
+        client.chat_postMessage(channel=user, text=msg)
 
 
 @slack_app.event("message")
