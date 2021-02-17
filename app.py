89b254f358813e5954239dbaa51194c2cb15d985
yankeestom <tom@yankeestom.com>
2021-02-17 15:42:51 -0600

    optional true

--- app.py
+++ app.py
@@ -169,7 +169,7 @@ async def command(ack, body, respond, client, logger):
                     "element": {
                         "type": "plain_text_input",
                         "action_id": "fng-action",
-                        "initial_value": " ",
+                        "optional": True,
                         "placeholder": {
                             "type": "plain_text",
                             "text": "List new FNG names separated by commas"
