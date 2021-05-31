ea28620469389e68a3197e3b737911ce7f8e111e
Trent Jones <JonesTD@legal.regn.net>
2021-05-31 10:52:51 -0400

    only attempt to send email if we have the things.

--- sendmail.py
+++ sendmail.py
@@ -4,26 +4,31 @@ from decouple import config
 
 
 def send(subject, recipient, body):
-    # defaults to gmail
+    # was older gmail variables set?
     gmail_user = config('GMAIL_USER')
     gmail_password = config('GMAIL_PWD')
 
-    # overrides
-    email_server = config('EMAIL_SERVER', default='smtp.gmail.com')
-    email_server_port = config('EMAIL_SERVER_PORT', default=465)
-    email_user = config('EMAIL_USER', default=gmail_user)
-    email_password = config('EMAIL_PASSWORD', default=gmail_password)
+    email_server = config('EMAIL_SERVER')
+    email_server_port = config('EMAIL_SERVER_PORT')
+    email_user = config('EMAIL_USER')
+    email_password = config('EMAIL_PASSWORD')
+
+    if gmail_user:
+        email_server = 'smtp.gmail.com'
+        email_server_port = 465
+        email_user = gmail_user
+        email_password = gmail_password
 
     msg = EmailMessage()
     msg.set_content(body)
 
     msg['Subject'] = subject
-    msg['From'] = gmail_user
+    msg['From'] = email_user
     msg['To'] = recipient
 
-    server = smtplib.SMTP_SSL(email_server, email_server_port)
-    server.ehlo()
-
-    server.login(email_user, email_password)
-    server.send_message(msg)
-    server.close()
+    if email_server and email_server_port and email_user and email_password and recipient:
+        server = smtplib.SMTP_SSL(email_server, email_server_port)
+        server.ehlo()
+        server.login(email_user, email_password)
+        server.send_message(msg)
+        server.close()
