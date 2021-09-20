80fe123ac5f00cc4dd45aec0122d67e328519904
Trent <trenton.jones@gmail.com>
2021-09-19 22:29:21 -0400

    allow configurable email server (#9)
    * allow configurable email server
    
    * update readme and .env-
    
    * only attempt to send email if we have the things.
    
    * recommended changes.
    
    * Add email_server config default to prevent error and update error message
    
    Co-authored-by: Trent Jones <JonesTD@legal.regn.net>
    Co-authored-by: willhlaw <will.lawrence@gmail.com>

--- sendmail.py
+++ sendmail.py
@@ -2,19 +2,23 @@ import smtplib
 from email.message import EmailMessage
 from decouple import config
 
+
 def send(subject, recipient, body):
-    gmail_user = config('GMAIL_USER')
-    gmail_password = config('GMAIL_PWD')
+    email_server = config('EMAIL_SERVER', 'smtp.gmail.com')
+    email_server_port = config('EMAIL_SERVER_PORT', 465)
+    email_user = config('EMAIL_USER')
+    email_password = config('EMAIL_PASSWORD')
 
     msg = EmailMessage()
     msg.set_content(body)
 
     msg['Subject'] = subject
-    msg['From'] = gmail_user
+    msg['From'] = email_user
     msg['To'] = recipient
 
-    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-    server.ehlo()
-    server.login(gmail_user, gmail_password)
-    server.send_message(msg)
-    server.close()
+    if email_server and email_server_port and email_user and email_password and recipient:
+        server = smtplib.SMTP_SSL(email_server, email_server_port)
+        server.ehlo()
+        server.login(email_user, email_password)
+        server.send_message(msg)
+        server.close()
