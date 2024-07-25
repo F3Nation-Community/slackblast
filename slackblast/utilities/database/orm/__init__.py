0ebc07c494a5070c83880fc27d602d5055c168e0
Evan Petzoldt <evan.petzoldt@protonmail.com>
2024-07-25 16:40:31 -0500

    adding curated backblasts

--- slackblast/utilities/database/orm/__init__.py
+++ slackblast/utilities/database/orm/__init__.py
@@ -106,6 +106,7 @@ class Backblast(BaseClass, GetDBClass):
     coq_user_id: Mapped[Optional[str45]]
     pax_count: Mapped[Optional[int]]
     backblast: Mapped[Optional[longtext]]
+    backblast_curated: Mapped[Optional[longtext]]
     fngs: Mapped[Optional[str45]]
     fng_count: Mapped[Optional[int]]
     json: Mapped[Optional[dict[str, Any]]]
