58341f0899682f4eb5852cb23fa45becab25ebe0
Evan Petzoldt <evan.petzoldt@protonmail.com>
2025-07-22 06:27:50 -0500

    updating announcement

--- slackblast/utilities/announcements.py
+++ slackblast/utilities/announcements.py
@@ -8,23 +8,12 @@ from slack_sdk import WebClient
 from utilities.database import DbManager
 from utilities.database.orm import PaxminerRegion, Region
 
-msg = "Hello, {region}! This is Moneyball, lead developer of the Slackblast app. I wanted to make you aware of updates "
-"to Slackblast over the last few months:\n\n"
-msg += ":left_speech_bubble: *Welcome Messages!* - We've added a new feature that allows you to send a welcome message "
-"to new members as they join your Slack space. This is a great way to introduce new members to your region and let them"
-"know how to navigate your space. Use `/config-welcome-message` to set it up.\n\n"
-msg += ":runner: *Strava Integration!* - Allows Strava PAX to transfer the backblast notes over to Strava, while "
-"gathering workout data (calories, distance) from linked devices. Go to `/config-slackblast` to enable.\n\n"
-msg += ":bar_chart: *Custom Fields!* - Does your region want to track something specific in your backblasts (burpee "
-"count, gloom index, etc.)? You can create your own fields through the Custom Field menu in `/config-slackblast`.\n\n"
-msg += ":sports_medal: *Weaselbot Achievements (new today)!* - For those using <https://github.com/F3Nation-Community"
-"/weaselbot|Weaselbot>, you can now assign achievements to PAX directly without having to create a whole backblast "
-"for it. Use the `/tag-achievement` command to get started.\n\n"
-msg += ":mechanic: *Under the Hood!* - Things like rich text input for backblasts, boyband image support, and better "
-"error handling.\n\n"
-msg += "\nIf you have any questions or issues, the best way to reach out is on the #paxminer-and-slackblast channel in "
-"the Nation space.\n"
+msg = "Hello, {region}! This is Moneyball, lead developer of the Slackblast app. I wanted to make you aware of a couple known issues in Slack right now that probably has affected your slackblast usage:\n\n"
+msg += ":warning: *Tagging* - Particularly on Android phones, you've probably noticed that you can only tag other PAX with their full name, not their display / F3 name\n\n"
+msg += ":warning: *Errors while using emojis* - Particularly on iOS devices, editing moleskines that have emojis will result in an error when you try to submit\n\n"
+msg += "\nBoth of these are known issues that I've bubbled up to Slack support. Unfortunately, there's nothing we can do but wait at this point. To avoid the second issue, I would avoid the use of emojis for now.\n"
 msg += "\n~ :moneybag: :baseball:"
+msg += "\n\nPS don't forget to tune in to the F3 Nation State of the Nation tonight at 8pm EST! There will be some info on a lot of cool stuff happening in the F3 tech space that you won't want to miss: https://f3nation.com/sotn"
 
 
 def send(client: WebClient, body: dict, logger: Logger, context: dict, region_record: Region):
